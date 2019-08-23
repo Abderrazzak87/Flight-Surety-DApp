4861403959c9edaad56bb5005df18bac16a65855
Abderrazzak87 <abd.benabdallah@gmail.com>
2019-08-24 00:16:55 +0200

    EVO#1 : Add the truffle.js configuration file  Author : Abderrazzak BENABDALLAH

---
+++ truffle.js
@@ -0,0 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "crunch space proud author rare sample foil they awake twice fetch brick";
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      },
+      network_id: '*',
+      gas: 9999999
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24"
+    }
+  }
+};
+
