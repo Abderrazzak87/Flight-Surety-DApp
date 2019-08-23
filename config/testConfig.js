dd27812a6df75206fa46306177f76e41f0b5365b
Abderrazzak87 <abd.benabdallah@gmail.com>
2019-08-24 00:14:45 +0200

    EVO#1 : Add the testConfig.js file  Author : Abderrazzak BENABDALLAH

---
+++ config/testConfig.js
@@ -0,0 +1,29 @@
+
+var FlightSuretyApp = artifacts.require("FlightSuretyApp");
+var FlightSuretyData = artifacts.require("FlightSuretyData");
+var BigNumber = require('bignumber.js');
+
+var Config = async function(accounts) {
+    
+    // These test addresses are useful when you need to add
+    // multiple users in test scripts
+
+    let owner = accounts[0];
+    let firstAirline = accounts[1];
+
+    let flightSuretyData = await FlightSuretyData.new(web3.utils.utf8ToHex('FirstAirline'));
+    let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address);
+
+    
+    return {
+        owner: owner,
+        firstAirline: firstAirline,
+        weiMultiple: (new BigNumber(10)).pow(18),
+        flightSuretyData: flightSuretyData,
+        flightSuretyApp: flightSuretyApp
+    }
+}
+
+module.exports = {
+    Config: Config
+};
